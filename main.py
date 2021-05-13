717f7816cc1ac5cf1ad2316d4913d099d7a930cf
Flikskek <zyzyfyk@gmail.com>
2021-05-13 19:38:25 +0300

    btc usd

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from store import tok
+from pycbrf import ExchangeRates
+from datetime import datetime
 
 import requests,time,json
 
-
+rates = ExchangeRates(datetime.now())
 
 session = vk_api.VkApi(token=tok)
 
-def get_price():
-  url = f"https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC"  
-  print(url)
-  j = requests.get(url)
+def get_price_usd():
+    url_usd = f"http://apilayer.net/api/live?access_key=f70d8094d6921b42003c41652d03c31b&currencies=RUB,GBP,CAD,PLN&source=USD&format=1"
+    print(url_usd)
+    u=requests.get(url_usd)
+    datausd = json.loads(u.text)
+    priceu = str(datausd["quotes"]["USDRUB"])
+    return 'Курс доллара в рублях 1USD = ' + priceu + "💰"
+
+
+def get_price_btc():
+  url_btc = f"https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC"  
+  print(url_btc)
+  j = requests.get(url_btc)
   data = json.loads(j.text)  
   price = str(data['result']['Ask'])
-  return 'Курс биткоина в долларах: ' + price + "💰"
+  return'Курс биткоина в долларах 1BTC = ' + price + "💰"
 
 def send_message(user_id, message, keyboard=None):
     post = {
@@ -36,15 +47,25 @@ for event in VkLongPoll(session).listen():
         text = event.text.lower()
         user_id = event.user_id
 
+        
+        
+
         if text == 'начать':
             keyboard = VkKeyboard()
             keyboard.add_button('BitCoin💳',VkKeyboardColor.POSITIVE)
+            keyboard.add_button('USD💵',VkKeyboardColor.NEGATIVE)
 
-            send_message(user_id,'Привет, пока что я могу показывать тебе курс биткоина, но это пока....',keyboard)
+            send_message(user_id,'Привет, пока что я могу показывать тебе курс биткоина и доллара, но это пока....',keyboard)
         elif text == 'bitcoin💳':
             keyboard = VkKeyboard()
             keyboard.add_button('BitCoin💳',VkKeyboardColor.POSITIVE)
+            keyboard.add_button('USD💵',VkKeyboardColor.NEGATIVE)
+            send_message(user_id,get_price_btc(),keyboard)
+        elif text == 'usd💵':
+            keyboard = VkKeyboard()
+            keyboard.add_button('BitCoin💳',VkKeyboardColor.POSITIVE)
+            keyboard.add_button('USD💵',VkKeyboardColor.NEGATIVE)
+            send_message(user_id,get_price_usd(),keyboard)
 
-            send_message(user_id,get_price(),keyboard)
 
         
